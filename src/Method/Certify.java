1d36f9f8e9c3b73b628391d648a72625738c3583
redbeers <tkdqls0077@gmail.com>
2021-01-26 13:23:20 +0900

    .

---
+++ src/Method/Certify.java
@@ -0,0 +1,30 @@
+package Method;
+
+public class Certify {
+	
+	private int certifyNum;
+	private String customerID;
+	private int birthday;
+	public int getCertifyNum() {
+		return certifyNum;
+	}
+	public void setCertifyNum(int certifyNum) {
+		this.certifyNum = certifyNum;
+	}
+	public String getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+	public int getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(int birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+	
+
+}
