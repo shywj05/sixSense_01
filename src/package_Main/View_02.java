2e61774da91b31dd68d55663826e64bdd3c7ffd1
shywj05 <shywj05@gmail.com>
2021-01-26 20:25:20 +0900

    .

--- src/package_Main/View_02.java
+++ src/package_Main/View_02.java
@@ -2,151 +2,136 @@ package package_Main;
 
 public class View_02 {
 	/**
-	 * 
+	 * 관리자메인-고객의 소리
 	 * @author 성원제1
 	 * @param
 	 */
-	// 관리자메인-고객의 소리
 	private void question() {
 
 	}
 
 	/**
-	 * 
+	 * 관리자메인-고객의 소리-답변등록
 	 * @author 성원제2
 	 * @param
 	 */
-	// 관리자메인-고객의 소리-답변등록
 	private void addQuestion() {
 
 	}
 
 	/**
-	 * 
+	 * 관리자메인-고객의 소리-답변수정
 	 * @author 성원제3
 	 * @param
 	 */
-	// 관리자메인-고객의 소리-답변수정
 	private void updateQuestion() {
 
 	}
 
 	/**
-	 * 
+	 * 관리자메인-고객의 소리-답변삭제
 	 * @author 성원제4
 	 * @param
 	 */
-	// 관리자메인-고객의 소리-답변삭제
 	private void removeQuestion() {
 
 	}
 
 	/**
-	 * 
+	 * 관리자메인-주문내역
 	 * @author 성원제5
 	 * @param
 	 */
-	// 관리자메인-주문내역
 	private void orderedNum() {
 
 	}
 
 	/**
-	 * 
+	 * 주문내역-환분내역
 	 * @author 성원제6
 	 * @param
 	 */
-	// 주문내역-환분내역
 	private void refundDetail() {
 
 	}
 
 	/**
-	 * 
+	 * 관리자메인-주문내역-매출현황
 	 * @author 성원제7
 	 * @param
 	 */
-	// 관리자메인-주문내역-매출현황
 	private void saleDetail() {
 
 	}
 
 	/**
-	 * 
+	 * 회원 메인화면
 	 * @author 성원제8
 	 * @param
 	 */
-	// 회원 메인화면
 	private void customerMain() {
 
 	}
 
 	/**
-	 * 
+	 * 회원 메인화면-카테고리
 	 * @author 성원제9
 	 * @param
 	 */
-	// 회원 메인화면-카테고리
 	private void category() {
 
 	}
 
 	/**
-	 * 
+	 * 회원 메인화면-카테고리-리스트
 	 * @author 성원제10
 	 * @param
 	 */
-	// 회원 메인화면-카테고리-리스트
 	private void categoryList() {
 
 	}
 
 	/**
-	 * 
+	 * 회원 메인화면-카테고리-리스트-수량
 	 * @author 성원제11
 	 * @param
 	 */
-	// 회원 메인화면-카테고리-리스트-수량
 	private void categoryListMount() {
 
 	}
 
 	/**
-	 * 
+	 * 회원 메인화면-카테고리-리스트-수량-결재
 	 * @author 성원제12
 	 * @param
 	 */
-	// 회원 메인화면-카테고리-리스트-수량-결재
 	private void categoryOrder() {
 
 	}
 
 	/**
-	 * 
+	 * 회원 메인화면-카테고리-리스트-수량-바구니담기
 	 * @author 성원제13
 	 * @param
 	 */
-	// 회원 메인화면-카테고리-리스트-수량-바구니담기
 	private void categoryAdd() {
 
 	}
 
 	/**
-	 * 
+	 * 회원 메인화면-장바구니보기
 	 * @author 성원제14
 	 * @param
 	 */
-	// 회원 메인화면-장바구니보기
 	private void cartList() {
 
 	}
 
 	/**
-	 * 
+	 * 회원 메인화면-장바구니보기-결재
 	 * @author 성원제15
 	 * @param
 	 */
-	// 회원 메인화면-장바구니보기-결재
 	private void cartOrder() {
 
 	}
