991a14a70e114f2e9c7b4e8cf2512f2b38fa8feb
redbeers <tkdqls0077@gmail.com>
2021-01-26 13:24:28 +0900

    .

---
+++ src/Method/CustomerVO.java
@@ -0,0 +1,77 @@
+package Method;
+
+//손님
+public class CustomerVO {
+	private String customerID;//손님 아이디pk
+	private String password;//손님 비밀번호
+	private String name;//손님 이름
+	private String birthday;//주민번호	
+	private String address;//손님 주소
+	private int point;//돈 충전
+	
+	
+	public String getCustomerID() {
+		return customerID;
+	}
+
+
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public int getPoint() {
+		return point;
+	}
+
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+
+	@Override
+	public String toString() {//아이디, 이름 반환
+		return "아이디 : " + customerID +"이름 : "+ name ;
+	}	
+}
---
+++ src/Method/Datavase.java
@@ -0,0 +1,79 @@
+package Method;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Scanner;
+
+public class Datavase {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//손님 저장 할곳
+		List<CustomerVO> cusVO = new ArrayList<>();
+		//손님 생성
+		CustomerVO c1= new CustomerVO();
+		
+		c1.setCustomerID("aaaa1");
+		c1.setPassword("00001");
+		c1.setName("박상빈");
+		c1.setAddress("대전");
+		c1.setPoint(10000);
+		
+		CustomerVO c2= new CustomerVO();
+		
+		c2.setCustomerID("aaaa2");
+		c2.setPassword("00001");
+		c2.setName("민선");
+		c2.setAddress("대전");
+		c2.setPoint(10000);
+		
+		
+		CustomerVO c3= new CustomerVO();
+		
+		c3.setCustomerID("aaaa3");
+		c3.setPassword("00001");
+		c3.setName("원제");
+		c3.setAddress("대전");
+		c3.setPoint(10000);
+		
+		
+		CustomerVO c4= new CustomerVO();
+		
+		c4.setCustomerID("aaaa4");
+		c4.setPassword("00001");
+		c4.setName("세웅");
+		c4.setAddress("대전");
+		c4.setPoint(10000);
+		
+		CustomerVO c5= new CustomerVO();
+		
+		c5.setCustomerID("aaaa6");
+		c5.setPassword("00001");
+		c5.setName("학재");
+		c5.setAddress("대전");
+		c5.setPoint(50000);
+		
+		
+		//cusVO에 손님 저장
+		cusVO.add(c1);
+		cusVO.add(c2);
+		cusVO.add(c3);
+		cusVO.add(c4);
+		cusVO.add(c5);
+		System.out.println(cusVO.toString()+"\r");
+		
+		System.out.println(cusVO.size());
+		
+		
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Calendar time = Calendar.getInstance();
+		
+		String format_time1 = format1.format(time.getTime());
+		
+		System.out.println(format_time1);
+	}
+
+}
