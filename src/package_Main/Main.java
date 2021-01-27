deb2ee1295cb190a21baf0d6f15e2fd8732feb2b
redbeers <tkdqls0077@gmail.com>
2021-01-27 09:13:42 +0900

    ..

--- src/package_Main/Main.java
+++ src/package_Main/Main.java
@@ -1,6 +1,9 @@
 package package_Main;
 
 public class Main {
-
-	
+	public static void main(String[] args) {
+	View_00 view = new View_00();
+	view.mainScreen();
+	System.out.println("🪐어서오세요 🌍");
+	}
 }
---
+++ src/package_Main/View_00.java
@@ -0,0 +1,143 @@
+package package_Main;
+
+public class View_00 {
+
+	/**
+	 *  1. 로그인
+		2. 회원가입
+	 * @author 박상빈1
+	 * @param
+	 */
+	void mainScreen() {
+		
+	}
+
+	/**
+	 * 
+	 * @author 박상빈2
+	 * @param
+	 */
+	private void login() {
+
+	}
+
+	/**
+	 * 관리자 메인화면
+	 * @author 박상빈3
+	 * @param
+	 */
+	
+	private void adminMain() {
+
+	}
+
+	/**
+	 * 관리자메인-상품관리
+	 * @author 박상빈4
+	 * @param
+	 */
+	
+	private void product() {
+
+	}
+
+	/**
+	 * 관리자메인-상품관리-상품추가
+	 * @author 박상빈5
+	 * @param
+	 */
+	
+	private void addProduct() {
+
+	}
+
+	/**
+	 * 관리자메인-상품관리-상품삭제
+	 * @author 박상빈6
+	 * @param
+	 */
+	
+	private void removeProduct() {
+
+	}
+
+	/**
+	 * 관리자메인-상품관리-상품수정
+	 * @author 박상빈7
+	 * @param
+	 */
+	
+	private void setProduct() {
+
+	}
+
+	/**
+	 * 관리자메인-재고관리
+	 * @author 박상빈8
+	 * @param
+	 */
+	
+	private void stock() {
+
+	}
+
+	/**
+	 * 관리자메인-재고추가
+	 * @author 박상빈9
+	 * @param
+	 */
+	
+	private void addStock() {
+
+	}
+
+	/**
+	 * 관리자메인-재고삭제
+	 * @author 박상빈10
+	 * @param
+	 */
+	
+	private void removeStock() {
+
+	}
+
+	/**
+	 * 관리자메인-이벤트관리
+	 * @author 박상빈11
+	 * @param
+	 */
+	
+	private void event() {
+
+	}
+
+	/**
+	 * 관리자메인-이벤트관리-이벤트추가
+	 * @author 박상빈12
+	 * @param
+	 */
+	
+	private void addEvent() {
+
+	}
+
+	/**
+	 * 관리자메인-이벤트관리-이벤수정
+	 * @author 박상빈13
+	 * @param
+	 */
+	
+	private void updateEvent() {
+
+	}
+
+	/**
+	 * 관리자메인-이벤트관리-이벤삭제
+	 * @author 박상빈14
+	 * @param
+	 */
+	
+	private void removeEvent() {
+
+	}
+}
